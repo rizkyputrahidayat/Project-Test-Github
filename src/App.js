abd7287b67e93e8f4b2f98663ed6a98406e2d326
rizkyputrahidayat <rizkyptr.h@gmail.com>
2021-08-25 16:07:01 +0700

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 // import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    searchRepos();
+  };
+
+  function searchRepos() {
+    setLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${username}/repos`,
+    }).then(res => {
+      setLoading(false);
+      setRepos(res.data);
+    });
+  }
+
   return (
     <div className="page">
       <div className="landing-page-container">
@@ -13,7 +33,7 @@ function App() {
               placeholder="Nama Github"
               onChange={e => setUsername(e.target.value)}
             />
-            <button className="button" onClick={handleSubmit}>{loading ? "Pencarian..." : "Cari"}</button>
+            <button className="button" onClick={handleSubmit}>{loading ? "Pencarian" : "Search"}</button>
           </form>
         </div>
       </div>
