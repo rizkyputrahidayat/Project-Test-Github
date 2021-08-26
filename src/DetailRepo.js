a9b9c3616471f38ea24166bf70a411d36a23885e
rizkyputrahidayat <rizkyptr.h@gmail.com>
2021-08-26 12:44:08 +0700

    Updated Fix App

--- src/DetailRepo.js
+++ src/DetailRepo.js
@@ -8,14 +8,14 @@ function DetailRepo({details, loading}){
     }
 
     return(
-        <div className="repo-details-container">
+        <div className="head-1">
             <div className='details-row'>
-                <label className="label">Name : </label>
-                <span className="value">{details.name}</span>
+                <label className="label">Name</label>
+                <span className="value">: {details.name}</span>
             </div>
             <div className='details-row'>
-                <label className="label">Language : </label>
-                <span className="value">{details.language}</span>
+                <label className="label">Language </label>
+                <span className="value-1">: {details.language}</span>
             </div>
         </div>
     );
